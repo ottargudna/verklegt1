be0c8da8b5b4474af3590a87f29b86c55ca628bb
ottargudna <ottargudna@gmail.com>
2023-12-05 14:57:28 +0000

    creating functions for airplane logic and employee logic

--- logic_layer/airplane_logic.py
+++ logic_layer/airplane_logic.py
@@ -3,3 +3,12 @@ from Model.airplane import Airplane
 class Airplane_Logic:
     def __init__(self) -> None:
         pass
+
+    def register_airplane(self):
+        pass
+
+    def list_airplanes(self):
+        pass
+
+    def edit_airplane(self):
+        pass
--- logic_layer/employee_logic.py
+++ logic_layer/employee_logic.py
@@ -4,3 +4,21 @@ from Model.employee import Employee
 class Employee_Logic:
     def __init__(self) -> None:
         pass
+
+    def create_empolyee(self):
+        pass
+
+    def list_employees(self):
+        pass
+
+    def edit_employee(self):
+        pass
+
+    def check_working_status(self):
+        pass
+
+    def list_voyges_for_an_employee(self):
+        pass
+
+    def get_all_employees(self):
+        pass
