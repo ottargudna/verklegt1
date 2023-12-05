01e5b2c69ce6fd57e7126819ded89adf43554eff
vigdishelga <142886057+vigdishelga@users.noreply.github.com>
2023-12-05 15:43:42 +0000

    Gerði fallegra menu
    Er að vinna í því að gera fallegra menu með fallegri flugvél, ascii artið fyrir nan air vill samt ekki fara í miðjuna.

--- UI_layer/Destinations_menu_UI.py
+++ UI_layer/Destinations_menu_UI.py
@@ -1,23 +1,54 @@
+import pyfiglet as pfg
 from Model.desinations import Desinations
+import string
 
-STARS = 50 * "*"
+BORDER = 109 * "="
+ASCII_NANAIR = pfg.figlet_format("NaN AIR", font="slant")
 
 class Destiantions_menu_ui:
 
     def __init__(self) -> None:
-        pass
+        self.main_menu = f"""
+                                [1] List of flights to specific destinations
+                                [2] List of the most popular destinations
+                                [3] Register a new destination
+                                [4] Edit destinations
+{BORDER}
+        """
 
-    def menu_output(self):
-        print(STARS)
-        print("[1] List of flights to specific destinations")
-        print("[2] List of the most popular destinations")
-        print("[3] Register a new destination")
-        print("[4] Edit destinations")
-        print()
-        print("[B]ack to main menu  [Q]uit")
-        print(STARS)
+        self.options = f"""
+                                [B]ack to main menu     [Q]uit
+{BORDER}
+        """
 
-        self.input_prompt()
+        self.ascii_airplane = f"""
+{BORDER}
+                                                 | 
+                                                 |
+                                                 |
+                                               .-'-.
+                                              ' ___ '
+                                    ---------'  .-.  '---------
+                    _________________________'  '-'  '_________________________
+                     ''''''-|---|--/    \==][^',_m_,'^][==/    \--|---|-''''''
+                                   \    /  ||/   H   \||  \    /
+                                    '--'   OO   O|O   OO   '--'
+{BORDER}
+        """
+
+        self.ascii_nanair = f"""
+{ASCII_NANAIR}
+{BORDER}
+        """
+
 
     def input_prompt(self):
         return input("Enter your selection: ")
+    
+    def print_menu(self) -> str:
+        print(self.ascii_airplane)
+        print(self.ascii_nanair)
+        print(self.main_menu)
+        print(self.options)
+        print()
+        self.input_prompt()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from UI_layer.Employee_menu_UI import Employee_menu_ui
 from UI_layer.mainmenu_ui import MainMenu_ui
+from UI_layer.Destinations_menu_UI import Destiantions_menu_ui
 
 main_menu = MainMenu_ui()
 employee = Employee_menu_ui()
 
-main_menu.staff_member_menu()
+#main_menu.staff_member_menu()
+
+destinations_menu = Destiantions_menu_ui()
+destinations_menu.print_menu()
