740ee87fc90532f1b563cd9664544ba493be433b
kolbrunh <142886956+kolbrunh@users.noreply.github.com>
2023-12-06 11:23:57 +0000

    employee_data updated

--- data_layer/Employee_Data.py
+++ data_layer/Employee_Data.py
@@ -3,17 +3,17 @@ from Model.employee import Employee
 
 class Employee_Data:
     def __init__(self) -> None:
-        self.file_name = "files/employees.cvs"
+        self.file_name = "Files/crew.csv"
 
     def get_all_employees(self):
         list = []
-        with open(self.file_name, newline= " ", encoding = "utf-8") as csvfile:
+        with open(self.file_name, newline= '', encoding = "utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                list.append(Employee(row["name"], row ["social_security_number"], row ["home_address"], row ["phone_number"]))
+                list.append(Employee(row["nid"], row ["name"], row ["role"], row ["address"], row ["phone_nr"]))
         return list 
     
     def create_employee(self, employee):
-        with open(self.file_name, "w", newline= " ", encoding = " utf-8") as csvfile:
-            fieldnames = ["name"," social_security_number","home_address", "phone_number"]
+        with open(self.file_name, "a", newline= " ", encoding = " utf-8") as csvfile:
+            fieldnames = ["nid","name","role", "address", "phone_nr"]
             writer = csv.DictReader(csvfile,fieldnames = fieldnames)
