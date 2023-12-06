515da9cfdfd7d27790d7704208f1c79fcd718a30
OliverOrri <144351568+OliverOrri@users.noreply.github.com>
2023-12-06 10:45:53 +0000

    flights logic
    byrjaði að gera def föllin i Flight_Logic

--- logic_layer/flights_logic.py
+++ logic_layer/flights_logic.py
@@ -1,5 +1,22 @@
 from Model.flights import Flights
 
 class Flights_Logic:
+
     def __init__(self) -> None:
         pass
+
+    def destination(self):
+        pass
+
+    def register_flight(self):
+        pass
+
+    def get_flight_details(self):
+        pass
+
+    def departure_time(self):
+        pass
+
+    def flight_status(self):
+        pass
+    
