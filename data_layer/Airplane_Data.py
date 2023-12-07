d9e2f3e6e4ed1b47eedab03829d2df79454d703d
kolbrunh <142886956+kolbrunh@users.noreply.github.com>
2023-12-07 11:35:52 +0000

    Update Airplane_Data.py

--- data_layer/Airplane_Data.py
+++ data_layer/Airplane_Data.py
@@ -3,4 +3,18 @@ from Model.airplane import Airplane
 
 class Airplane_Data:
     def __init__(self) -> None:
-        pass
+        self.file_name = "Files/"
+
+
+    def get_all_airplanes(self):
+        list = []
+        with open(self.file_name, newline= '', encoding = "utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                list.append(Airplane(row["name"], row ["plane_type"], row ["manufacturer"], row ["number_of_passanger_seats"]))
+        return list 
+    
+    def create_airplanes(self, Airplane):
+        with open(self.file_name, "a", newline= " ", encoding = " utf-8") as csvfile:
+            fieldnames = ["name","plane_type","manufacturer", "number_of_passanger_seats"]
+            writer = csv.DictReader(csvfile,fieldnames = fieldnames) 
