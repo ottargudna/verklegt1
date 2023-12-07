a8adb8fc21695b849d1ceddf8246e743988351d9
kolbrunh <142886956+kolbrunh@users.noreply.github.com>
2023-12-07 11:36:20 +0000

    Update Destinations_Data.py

--- data_layer/Destinations_Data.py
+++ data_layer/Destinations_Data.py
@@ -3,4 +3,17 @@ from Model.desinations import Desinations
 
 class Destinations_Data:
     def __init__(self) -> None:
-        pass
+        self.file_name = "Files/destinations.csv"
+
+    def get_all_destinations(self):
+        list = []
+        with open(self.file_name, newline= '', encoding = "utf-8") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                list.append(Desinations(row["destination"], row ["airport"], row ["flight_time"], row["distance_from_iceland"]))
+        return list 
+    
+    def create_employee(self, employee):
+        with open(self.file_name, "a", newline= " ", encoding = " utf-8") as csvfile:
+            fieldnames = ["destination", "airport", "flight_time", "distance_from_iceland"]
+            writer = csv.DictReader(csvfile,fieldnames = fieldnames)
