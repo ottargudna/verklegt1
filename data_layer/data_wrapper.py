8206f20b9472632a0f4ad2859b6429e4433c78d0
kolbrunh <142886956+kolbrunh@users.noreply.github.com>
2023-12-08 17:10:41 +0000

    Update data_wrapper.py

--- data_layer/data_wrapper.py
+++ data_layer/data_wrapper.py
@@ -1,5 +1,4 @@
 
-
 from data_layer.Airplane_Data import Airplane_Data
 from data_layer.Destinations_Data import Destinations_Data
 from data_layer.Employee_Data import Employee_Data
@@ -9,11 +8,35 @@ from data_layer.Voyage_Data import Voyage_Data
 
 class Data_Wrapper:
     def __init__(self) -> None:
-        self.employee_logic = Employee_Data
-        self.airplane_logic = Airplane_Data
-        self.destination_logic = Destinations_Data
-        self.flight_logic = Flights_Data
-        self.voyage = Voyage_Data
+        self.employee_data = Employee_Data()
+        self.airplane_data = Airplane_Data()
+        self.destination_data = Destinations_Data()
+        self.flight_data = Flights_Data()
+        self.voyage_data= Voyage_Data()
 
+    def get_all_employees(self):
+        return self.employee_data.get_all_employees()
+    
+    def create_employees(self):
+        return self.employee_data.create_employee()
+    
+    def get_all_destinations(self):
+        return self.destination_data.get_all_destinations()
 
-    """Hérna á að koma öll föll sem eru notuð og returna þeim"""
+    def create_destination(self):
+        return self.destination_data.create_destination()
+
+    def get_all_voyages(self):
+        return self.voyage_data.get_all_voyages()
+    
+    def create_voyages(self):
+        return self.voyage_data.create_voyage()
+    
+    def get_all_airplanes(self):
+        return self.airplane_data.get_all_airplanes()
+    
+    def create_airplane(self):
+        return self.airplane_data.create_airplane()
+    
+
+    
