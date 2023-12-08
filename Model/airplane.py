9c0c1a910c7e286f4c27907093a0a0580952a7dc
ottargudna <ottargudna@gmail.com>
2023-12-08 11:13:20 +0000

    creating model for destinations and airplane

--- Model/airplane.py
+++ Model/airplane.py
@@ -1,5 +1,10 @@
 
 
 class Airplane:
-    def __init__(self):
-        pass
+    def __init__(self, plane_insignia, plane_type_id, capacity):
+        self.plane_insignia = plane_insignia
+        self.plane_type_id = plane_type_id
+        self.capacity = capacity
+
+    def __str__(self):
+        return f"Plane insignia: {self.plane_insignia}, Plane type ID: {self.plane_type_id}, Capacity: {self.capacity}"
