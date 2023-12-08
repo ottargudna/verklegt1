3f42abeb460b845d975b2f2758b72ff53e02d464
OliverOrri <144351568+OliverOrri@users.noreply.github.com>
2023-12-08 12:04:00 +0000

    updating logic layers

--- logic_layer/flights_logic.py
+++ logic_layer/flights_logic.py
@@ -2,21 +2,26 @@ from Model.flights import Flights
 
 class Flights_Logic:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, data_connection) -> None:
+        self.data_wrapper = data_connection
 
-    def destination(self):
-        pass
+    def destination(self, flight):        
+        self.data_wrapper.destination(flight)
 
     def register_flight(self):
-        pass
-
-    def get_flight_details(self):
-        pass
-
+        self.data_wrapper.register_flight()
+        
+    def get_flight_details(self, flight):
+        self.data_wrapper.get_flight_details(flight)
+        
     def departure_time(self):
-        pass
+        self.data_wrapper.departure_time.
 
     def flight_status(self):
-        pass
+        self.data_wrapper.
     
+    def create_flight(self):
+        self.data_wrapper.
+
+    def get_all_flights(self):
+        return self.data_wrapper.get_all_fligths()
--- logic_layer/voyage_logic.py
+++ logic_layer/voyage_logic.py
@@ -1,5 +1,15 @@
 from Model.voyage import Voyage
+from logic_layer.flights_logic import Flights
 
 class Voyage_Logic:
+    
     def __init__(self) -> None:
         pass
+
+    def create_voyage(self):
+        pass
+
+    def schedule_voyage(self):
+        pass
+
+    def 
