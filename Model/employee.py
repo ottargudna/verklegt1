bd410e99b8dd9a2468d9d586376b358d10800421
ottargudna <ottargudna@gmail.com>
2023-12-11 09:29:25 +0000

    changes in diary and employee model

--- Model/employee.py
+++ Model/employee.py
@@ -1,7 +1,7 @@
 
 
 class Employee:
-    def __init__(self, nid="1611042050", name="Óttar Atli", role="Kóngurinn", rank = "hæsta", licence = "dómarapróf", address = "Kleifakór 12", phone_nr = "spurðu snap", pref_nr = "snap", slot_param = "juju"):
+    def __init__(self, nid, name, role, rank, licence, address, phone_nr, email, homephone_nr):
         self.nid = nid
         self.name = name
         self.role = role
@@ -9,10 +9,10 @@ class Employee:
         self.licence = licence
         self.address = address
         self.phone_nr = phone_nr
-        self.pref_nr = pref_nr
-        self.slot_param = slot_param
+        self.email = email
+        self.homephone_nr = homephone_nr
 
     def __str__(self):
-        return f"ID number: {self.nid}, Name: {self.name}, Role: {self.role}, Rank: {self.rank}, Licence: {self.licence}, Addres: {self.address}, Phone number: {self.phone_nr}, Preferd phone number: {self.pref_nr}"
+        return f"ID number: {self.nid}, Name: {self.name}, Role: {self.role}, Rank: {self.rank}, Licence: {self.licence}, Addres: {self.address}, Phone number: {self.phone_nr}, email: {self.email}, homephone number:{self.homephone_nr}"
 
     
