e59a1f2b215d9fabc5795f369a590360655e30c7
ottargudna <ottargudna@gmail.com>
2023-12-11 09:53:56 +0000

    juju

--- debug_kolla.py
+++ debug_kolla.py
@@ -4,4 +4,4 @@ from data_layer.Employee_Data import Employee_Data
 data_class = Employee_Data()
 result = data_class.get_all_employees()
 for elem in result:
-    print(elem.name)
+    print(elem.name + " - " + elem.nid)
