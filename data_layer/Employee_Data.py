20bd362256e3af405202651e248d156d044d98ca
kolbrunh <142886956+kolbrunh@users.noreply.github.com>
2023-12-12 17:44:12 +0000

    Update Employee_Data.py

--- data_layer/Employee_Data.py
+++ data_layer/Employee_Data.py
@@ -16,11 +16,7 @@ class Employee_Data:
     
     def create_employee(self, employee):
         with open(self.file_name, "a", newline= " ", encoding = " utf-8") as csvfile:
-<<<<<<< HEAD
-            fieldnames = ["nid", "name", "role", "rank", "address", "phone_nr"]
-=======
             fieldnames = ["nid","name","role", "rank","licence", "address", "phone_nr"]
->>>>>>> ottar5
             writer = csv.DictWriter(csvfile,fieldnames = fieldnames)
             writer.writerow({"nid": employee.nid, "name": employee.name, "role": employee.role, "rank": employee.rank, "licence":employee.licence, "address": employee.address, "phone_nr": employee.phone_nr, "email": employee.email, "homephone_nr":employee.homephone.nr})
 
@@ -29,17 +25,20 @@ class Employee_Data:
         employees = self.get_all_employees() 
 
         for employee in employees: 
-            if employee.nid == employee.nid:
+            if employee.nid == updated_employee.nid:
                 #update the employee information
                 employee.role = updated_employee.role
                 employee.rank = updated_employee.rank
                 employee.address = updated_employee.address
                 employee.phone_nr = updated_employee.phone_nr
 
-                with open(self.file_name, "w+", newline = "", encoding = "utf-8") as csvfile:
+                with open(self.file_name, "w", newline = "", encoding = "utf-8") as csvfile:
                     fieldnames = ["nid", "name", "role", "rank", "address", "phone_nr"]
                     writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
                     writer.writeheader()
-                    writer.writerow(employee) 
+                    
+                    for employee in employees:
+                        writer.writerow({"role": employee.role, "rank": employee.rank, "address": employee.address, "phone_nr": employee.phone_nr})
+
 
                 
