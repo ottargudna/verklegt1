6c90d059ecd6707384f7039458580579df158bee
vigdishelga <142886057+vigdishelga@users.noreply.github.com>
2023-12-13 09:48:58 +0000

    Kláraði employee í ui
    Ég færði til og bjó til föll og er núna búin með employee klasann í ui layer

--- UI_layer/Employee_menu_UI.py
+++ UI_layer/Employee_menu_UI.py
@@ -1,7 +1,10 @@
 from Model.employee import Employee
-
+from UI_layer.mainmenu_ui import MainMenu_ui
 
 BORDER = 109 * "*"
+QUIT = "q"
+BACK = "b"
+EXIT_TEXT = "Goodbye :)"
 SELECTION_ONE = "1"
 SELECTION_TWO = "2"
 SELECTION_THREE = "3"
@@ -12,21 +15,23 @@ SELECTION_FIVE = "5"
 class Employee_menu_ui:
 
     def __init__(self) -> None:
+        self.main_menu = MainMenu_ui()
+        
         self.main_menu = f"""
-                                [1] List all employees
+                                [1] Employees
                                 [2] Pilot
                                 [3] Crew
         {BORDER}
         """
 
-        self.pilot_menu = f"""
+        self.pilot_manager = f"""
                                 [{SELECTION_ONE}] Register a new pilot
                                 [{SELECTION_TWO}] Edit information of a pilot
                                 [{SELECTION_THREE}] Search for a pilot
-                                [{SELECTION_FOUR}] List pilots
+                                [{SELECTION_FOUR}] List all pilots
 {BORDER}
         """
-        self.crew = f"""
+        self.crew_manager = f"""
                                 [{SELECTION_ONE}] Register a new crew member
                                 [{SELECTION_TWO}] Edit information of a crew member
                                 [{SELECTION_THREE}] Search for a crew member
@@ -34,3 +39,190 @@ class Employee_menu_ui:
 {BORDER}
         """
 
+        self.pilot_shift_supervisor = f"""
+                                [{SELECTION_ONE}] Search for a pilot
+                                [{SELECTION_TWO}] List all pilots
+{BORDER}
+        """
+
+        self.crew_shift_supervisor = f"""
+                                [{SELECTION_ONE}] Search for a crew member
+                                [{SELECTION_TWO}] List all crew members
+{BORDER}
+        """
+
+        self.employees_menu = f"""
+                                [{SELECTION_ONE}] List all employees
+                                [{SELECTION_TWO}] List information of an employee
+                                [{SELECTION_THREE}] List all employees not working on a given day
+                                [{SELECTION_FOUR}] List all employees working on a given day
+                                [{SELECTION_FIVE}] Printable work summary for an employee in a giving week
+{BORDER}
+        """
+    
+
+    def get_manager_employees_menu(self):
+        print(self.main_menu.ascii_nanair)
+        print(self.main_menu)
+        print(self.main_menu.options)
+        user_selection = self.main_menu.input_prompt()
+        
+        if user_selection == SELECTION_ONE: # Employees
+            self.get_employees()
+            
+        elif user_selection == SELECTION_TWO: # Pilot
+            self.get_manager_pilots()
+            
+        elif user_selection == SELECTION_THREE: # Crew
+            self.get_manager_crew()
+        
+        elif user_selection == QUIT:
+            print(EXIT_TEXT)
+            quit()
+        
+        else: # Go back
+            pass
+    
+
+    def get_employees(self):
+        print(self.main_menu.ascii_nanair)
+        print(self.employees_menu)
+        print(self.main_menu.options)
+        user_selection = self.main_menu.input_prompt()
+
+        if user_selection == SELECTION_ONE: # List all employees
+            pass
+
+        elif user_selection == SELECTION_TWO: # List information of an employee
+            pass
+
+        elif user_selection == SELECTION_THREE: # List all employees not working on a given day
+            pass
+
+        elif user_selection == SELECTION_FOUR: # List all employees working on a given day
+            pass
+
+        elif user_selection == SELECTION_FIVE: # Printable work summary for an employee in a giving week
+            pass
+
+        elif user_selection == QUIT:
+            print(EXIT_TEXT)
+            quit()
+        
+        else: # Go back
+            pass
+    
+
+    def get_manager_pilots(self):
+        print(self.main_menu.ascii_nanair)
+        print(self.pilot_manager)
+        print(self.main_menu.options)
+        user_selection = self.main_menu.input_prompt()
+
+        if user_selection == SELECTION_ONE: # Register a new pilot
+            pass
+
+        elif user_selection == SELECTION_TWO: # Edit information of a pilot
+            pass
+
+        elif user_selection == SELECTION_THREE: # Search for a pilot
+            pass
+
+        elif user_selection == SELECTION_FOUR: # List pilots
+            pass
+
+        elif user_selection == QUIT:
+            print(EXIT_TEXT)
+            quit()
+        
+        else: # Go back
+            pass
+
+
+    def get_manager_crew(self):
+        print(self.main_menu.ascii_nanair)
+        print(self.crew_manager)
+        print(self.main_menu.options)
+        user_selection = self.main_menu.input_prompt()
+
+        if user_selection == SELECTION_ONE: # Register a new crew member
+            pass
+
+        elif user_selection == SELECTION_TWO: # Edit information of a crew member
+            pass
+
+        elif user_selection == SELECTION_THREE: # Search for a crew member
+            pass
+
+        elif user_selection == SELECTION_FOUR: # List crew members
+            pass
+
+        elif user_selection == QUIT:
+            print(EXIT_TEXT)
+            quit()
+    
+        else: # Go back
+            pass
+        
+    
+    def get_shift_superviser_employees_menu(self):
+        print(self.main_menu.ascii_nanair)
+        print(self.main_menu)
+        print(self.main_menu.options)
+        user_selection = self.main_menu.input_prompt()
+
+        if user_selection == SELECTION_ONE: # Employees
+            self.get_employees()
+
+        elif user_selection == SELECTION_TWO: # Pilot
+            self.get_shift_superviser_pilots()
+
+        elif user_selection == SELECTION_THREE: # Crew
+            self.get_shift_superviser_crew()
+        
+        elif user_selection == QUIT:
+            print(EXIT_TEXT)
+            quit()
+    
+        else: # Go back
+            pass
+
+
+    def get_shift_superviser_pilots(self):
+        print(self.main_menu.ascii_nanair)
+        print(self.pilot_shift_supervisor)
+        print(self.main_menu.options)
+        user_selection = self.main_menu.input_prompt()
+
+        if user_selection == SELECTION_ONE: # Search for a pilot
+            pass
+
+        elif user_selection == SELECTION_TWO: # List all pilots
+            pass
+
+        elif user_selection == QUIT:
+            print(EXIT_TEXT)
+            quit()
+    
+        else: # Go back
+            pass
+
+
+    def get_shift_superviser_crew(self):
+        print(self.main_menu.ascii_nanair)
+        print(self.crew_shift_supervisor)
+        print(self.main_menu.options)
+        user_selection = self.main_menu.input_prompt()
+
+        if user_selection == SELECTION_ONE: # Search for a crew member
+            pass
+
+        elif user_selection == SELECTION_TWO: # List all crew members
+            pass
+
+        elif user_selection == QUIT:
+            print(EXIT_TEXT)
+            quit()
+    
+        else: # Go back
+            pass
