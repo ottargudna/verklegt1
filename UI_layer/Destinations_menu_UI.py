05fa09e561568c9dc31d1a0f47008bf8e6c3a564
vigdishelga <142886057+vigdishelga@users.noreply.github.com>
2023-12-13 23:03:26 +0000

    Ui og ein lína í logic destinations

--- UI_layer/Destinations_menu_UI.py
+++ UI_layer/Destinations_menu_UI.py
@@ -1,17 +1,17 @@
 from Model.desinations import Desinations
 import UI_layer.constants
+from logic_layer.logic_wrapper import Logic_Wrapper
 import string
 
 class Destiantions_menu_ui:
 
     def __init__(self) -> None:
         self.const = UI_layer.constants
+        self.logic_wrapper = Logic_Wrapper()
     
     def get_manager_destinations(self):
-        print(self.main_menu.ascii_nanair)
-        print(self.manager_menu)
-        print(self.main_menu.options)
-        user_selection = self.main_menu.input_prompt()
+        print(self.const.destinations_menu)
+        user_selection = self.const.input_selection()
 
         if user_selection == self.const.ONE: # Register a new destination
             self.register_new_destination()
@@ -30,7 +30,15 @@ class Destiantions_menu_ui:
             pass
     
     def register_new_destination(self):
-        pass
+        destination_name = input("Enter the country: ")
+        destination_airport = input("Enter the initials of the airport: ")
+        destination_flight_time = input("Enter the flight time: ")
+        destination_distance = input("Enter the distance from Iceland: ")
+        destination_contact_name = input("Enter the contact name: ")
+        destination_contact_nr = input("Enter the contact emergency number: ")
+
+        destination = destination_name, destination_airport, destination_flight_time, destination_distance, destination_contact_name, destination_contact_nr
+        return self.logic_wrapper.data_wrapper.create_destination(destination)
 
     def edit_destanation(self):
         pass
