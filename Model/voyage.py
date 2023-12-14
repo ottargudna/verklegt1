0ac06b3c48c2022c31a59e9af9cdb9c4e2295040
kolbrunh <142886956+kolbrunh@users.noreply.github.com>
2023-12-14 10:32:29 +0000

    Update voyage.py

--- Model/voyage.py
+++ Model/voyage.py
@@ -1,6 +1,7 @@
 
 class Voyage:
-    def __init__(self, dep_from, arr_at, aircraft_id, date_out, date_home, captain, copilot, flight_service_manager, flight_attendant1, flight_attendant2):
+    def __init__(self, voyage_number, dep_from, arr_at, aircraft_id, date_out, date_home, captain, copilot, flight_service_manager, flight_attendant1, flight_attendant2):
+        self.voyage_number = voyage_number
         self.dep_from = dep_from
         self.arr_at = arr_at
         self.aircraft_id = aircraft_id
@@ -14,4 +15,4 @@ class Voyage:
     
 
     def __str__(self) -> str:
-        return f"Departure from: {self.dep_from}, Arriving at: {self.arr_at}, Aircraft ID: {self.aircraft_id}, Date flying out: {self.date_out}, Date flying home: {self.date_home}, Captain: {self.catain}, Copilot: {self.copilot}, Flight service manager: {self.flight_service_maneger}, Flight attendents: {self.flight_attendant1} and {self.flight_attendant2}"
+        return f"Voyage_number: {self.voyage_number}, Departure from: {self.dep_from}, Arriving at: {self.arr_at}, Aircraft ID: {self.aircraft_id}, Date flying out: {self.date_out}, Date flying home: {self.date_home}, Captain: {self.catain}, Copilot: {self.copilot}, Flight service manager: {self.flight_service_maneger}, Flight attendents: {self.flight_attendant1} and {self.flight_attendant2}"
