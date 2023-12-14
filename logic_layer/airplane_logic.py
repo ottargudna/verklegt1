aa2a80e4fd38deaa4310306862447e2a7b5e86e8
vigdishelga <142886057+vigdishelga@users.noreply.github.com>
2023-12-14 17:12:05 +0000

    kom conflict vonandi skemmdi eg ekkert

--- logic_layer/airplane_logic.py
+++ logic_layer/airplane_logic.py
@@ -9,28 +9,33 @@ class Airplane_Logic:
     def create_airplane(self, airplane):
         """Takes in airplane object and forwards it to the data layer"""
         check_existing_airplanes = self.airplane_already_exist(airplane)
+        if check_existing_airplanes == True:
         if check_existing_airplanes == False:
             self.datawrapper.create_airplane(airplane)
         else:
-<<<<<<< Updated upstream
             return False
-        #if false enter another name and return a str try another name
-
-    def get_all_airplanes(self):
-        """Gets all airplane"""
-=======
-            self.datawrapper.create_airplane(airplane)
+        else:
+            self.data_wrapper.create_airplane(airplane)
         #if true enter another name and return a str try another name
 
     def get_all_airplanes(self):
         """Gets all airplanes"""
->>>>>>> Stashed changes
+        self.datawrapper.get_all_airplanes()
+
+    def airplane_already_exist(self, airplane):
+        every_airplane = self.datawrapper.get_all_airplanes()
+        """Gets all airplane"""
+        self.datawrapper.create_airplane(airplane)
+        #if true enter another name and return a str try another name
+
+    def get_all_airplanes(self):
+        """Gets all airplanes"""
         self.datawrapper.get_all_airplanes()
 
     def airplane_already_exist(self, airplane):
         every_airplane = self.datawrapper.get_all_airplanes()
 
         for plane in every_airplane:
-            if airplane[0] == plane.plane_insignia:
+            if airplane.plane_insignia == plane.plane_insignia:
                 return True # already exists
         
