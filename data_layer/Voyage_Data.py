de5dbbfa10d709118bc633b53cd91378694dcd64
kolbrunh <142886956+kolbrunh@users.noreply.github.com>
2023-12-14 10:32:19 +0000

    Update Voyage_Data.py

--- data_layer/Voyage_Data.py
+++ data_layer/Voyage_Data.py
@@ -11,11 +11,35 @@ class Voyage_Data:
         with open(self.file_name, newline= '', encoding = "utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                list.append(Voyage(row["dep_from"], row ["arr_at"], row ["aircraft_id"], row ["date_out"], row ["date_home"], row ["captain"], row ["copilot"], row ["flight_service_manager"], row ["flight_attendant1"], row ["flight_attendant2"]))
+                list.append(Voyage(row["voyage_number"],row ["dep_from"], row ["arr_at"], row ["aircraft_id"], row ["date_out"], row ["date_home"], row ["captain"], row ["copilot"], row ["flight_service_manager"], row ["flight_attendant1"], row ["flight_attendant2"]))
         return list 
     
-    def create_voyage(self, Voyage):
-        with open(self.file_name, "a", newline= " ", encoding = " utf-8") as csvfile:
-            fieldnames = ["dep_from", "arr_at", "aircraft_id", "date_out", "date_home", "captain", "copilot", "flight_service_manager", "flight_attendant1", "flight_attendant2"]
-            writer = csv.DictReader(csvfile,fieldnames = fieldnames)
-            writer.writerow({"dep_from": Voyage.dep_from, "arr_at": Voyage.arr_at, "aircraft_id": Voyage.aircraft_id, "date_out": Voyage.date_out, "date_home": Voyage.date_home, "captain": Voyage.captain, "copilot": Voyage.copilot, "flight_service_manager": Voyage.flight_sevice_manager, "flight_attendant1": Voyage.flight_attendant1, "flight_attendant2": Voyage.flight_attendant2})
+
+    def create_voyage(self, voyage):
+        with open(self.file_name, 'a', newline= '', encoding = "utf-8") as csvfile:
+            fieldnames = ["voyage_number", "dep_from", "arr_at", "aircraft_id", "date_out", "date_home", "captain", "copilot", "flight_service_manager", "flight_attendant1", "flight_attendant2"]
+            writer = csv.DictWriter(csvfile,fieldnames = fieldnames)
+            writer.writerow({"voyage_number": voyage.voyage_number,"dep_from": voyage.dep_from, "arr_at": voyage.arr_at, "aircraft_id": voyage.aircraft_id, "date_out": voyage.date_out, "date_home": voyage.date_home, "captain": voyage.captain, "copilot": voyage.copilot, "flight_service_manager": voyage.flight_service_manager, "flight_attendant1": voyage.flight_attendant1, "flight_attendant2": voyage.flight_attendant2})
+
+
+    def update_voyages(self, updated_voyage):
+        voyages = self.get_all_voyages() 
+
+        for voyage in voyages: 
+            if voyage.voyage_number == updated_voyage.voyage_number:
+                #update the voyage crew information
+
+                voyage.captain = updated_voyage.captain
+                voyage.copilot = updated_voyage.copilot
+                voyage.flight_service_manager = updated_voyage.flight_service_manager
+                voyage.flight_attendant1 = updated_voyage.flight_attendant1
+                voyage.flight_attendant2 = updated_voyage.flight_attendant2
+
+                with open(self.file_name, "w+", newline = '', encoding = "utf-8") as csvfile:
+                    fieldnames = ["voyage_number", "dep_from", "arr_at", "aircraft_id", "date_out", "date_home", "captain", "copilot", "flight_service_manager", "flight_attendant1", "flight_attendant2"]
+                    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+                    writer.writeheader()
+                    
+                    for voyage in voyages:
+                        writer.writerow({"voyage_number": voyage.voyage_number, "dep_from": voyage.dep_from, "arr_at": voyage.arr_at, "aircraft_id": voyage.aircraft_id, "date_out": voyage.date_out, "date_home": voyage.date_home, "captain": voyage.captain, "copilot": voyage.copilot, "flight_service_manager": voyage.flight_service_manager, "flight_attendant1": voyage.flight_attendant1, "flight_attendant2": voyage.flight_attendant2})
+
