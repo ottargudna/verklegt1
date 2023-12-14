089f3aff9ea07f90d07f558533956104ea3cfaee
ottargudna <ottargudna@gmail.com>
2023-12-14 15:03:54 +0000

    juju gegur vel

--- ottar_debug.py
+++ ottar_debug.py
@@ -4,21 +4,30 @@ from data_layer.data_wrapper import Data_Wrapper
 from Model.employee import Employee
 
 
-#nid,name,role,rank,licence,address,phone_nr,email,homephone_nr
+#voyage_number,dep_from,arr_at,aircraft_id,date_out,date_home,captain,copilot,flight_service_manager,flight_attendant1,flight_attendant2
+
 data_class = Data_Wrapper()
 e = Employee
-'''e.nid = input("enter NID: ")
 
-e.role = input("role ")
-e.rank = input("rank ")
-e.licence = input("licence ")
-e.address = input("addres ")
-e.phone_nr = input("phone number ")
-e.email = input("email ")
-e.homephone_nr = input("homephone nr ")'''
+'''
+e.voyage_number = input("voyage_number ")
+e.dep_from = input("dep_from ")
+e.arr_at = input("arr_at ")
+e.aircraft_id = input("aircraft_id ")
+e.date_out = input("date_out ")
+e.date_home = input("date_home ")
+e.captain = input("captain  ")
+e.copilot = input("copilot ")
+e.flight_service_manager = input("flight_service_manager ")
+e.flight_attendant1 = input("flight_attendant1  ")
+e.flight_attendant2 = input("flight_attendant2  ")
+'''
+
+
+day = input("date")
 
 logic_class = Voyage_Logic(data_class)
-result = logic_class.generte_voyage_nr()
+result = logic_class.check_week(day)
 
-print(result)
+print(result[1])
 
